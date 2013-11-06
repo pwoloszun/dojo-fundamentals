abc115d219dde7c41b2612094461c695067c0c83
pwoloszun <piotr.woloszun@gmail.com>
2013-11-06 08:14:41 +0100

    v 1.2 fixed dom task

--- js/apps/dom/fancyNav.js
+++ js/apps/dom/fancyNav.js
@@ -1,3 +1,10 @@
 /*
 * FancyNavigator:
+* 1. w momencie gdy uzytkownik przesunie scrollbarem strony w dol/gore animujemy
+*   zmieniamy backgroundColor el ".js-menu" na kolor pomiedzy #f00, a #0f0 o procent
+*   o jaki przesunal scroll bar (np jesli strona ma 1000px wysokosci, a scrollbar przesunieto
+*   o 300px, to kolor powinien byc === 0.7 * #f00 + 0.3 * #0f0
+* 2. zaprogramowac obsluge zdarzenia 'scrollend'
+* 3. na zdarznie 'scroll end' uruchomic animacje, trwajaca 1200ms, ktora zjedzie elementem
+*   ".js-indicator" wzgledem el ".js-menu" o % o jaki przesunieto scrollbar
 * */
